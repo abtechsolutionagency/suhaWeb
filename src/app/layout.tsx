613c873211c4521f093ae67b9f099b2c6ca475ce
abdulbasit <proabdulbasit.me@gmail.com>
2024-12-02 14:56:28 +0500

    update code

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+
+"use client"
+import Wrapper from "@/layouts/Wrapper";
+import "../styles/style.css";
+import "../styles/style.scss";
+
+import store from "@/redux/store";
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en" theme-color="dark">
+			<head>
+				<title>Suha - Multipurpose Ecommerce Mobile Next js Template</title>
+				<link
+					href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap"
+					rel="stylesheet"
+				/>
+
+			</head>
+
+			<body>
+				<Provider store={store}>
+					{children}
+				</Provider> 
+				
+
+			</body>
+		</html>
+	);
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import MainHome from "@/components/MainHome";
+import Wrapper from "@/layouts/Wrapper";
+import React from "react";
+
+export const metadata = {
+	title: "Suha - Multipurpose Ecommerce Mobile Next js Template",
+};
+
+const index = () => {
+	return (
+		<Wrapper>
+			<MainHome />
+		</Wrapper>
+	);
+};
+
+export default index;
